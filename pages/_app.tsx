bd399eeb595a9167235f9a3723069173839abad3
s13bp <86206902+s13bp@users.noreply.github.com>
2023-07-31 01:11:33 +0300

    f

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,11 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
-import { Inter } from 'next/font/google';
+import { Raleway } from 'next/font/google';
 
 import '@/styles/globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Raleway({ subsets: ['latin'] });
 
 function App({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
